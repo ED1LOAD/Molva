93a465b51cc793ac46ecc748235c453cb88a3d91
Nikita Dankov <dankov.ns@phystech.edu>
2024-06-06 18:21:55 +0300

    basic onboarding

--- app/lib/features/onboarding/view/onboarding_screen.dart
+++ app/lib/features/onboarding/view/onboarding_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:app/features/onboarding/widgets/view.dart';
 
@@ -11,23 +12,10 @@ class OnboardingScreen extends StatefulWidget {
 class _OnboardingScreenState extends State<OnboardingScreen> {
   final PageController _pageController = PageController();
 
-  final List<OboardContent> _onboardingPages = [
-    OboardContent(
-      title: 'Исследуйте новый канал привлечения сотрудников',
-      description: 'Размещайте вакансию или привлекайте кандидатов',
-      button: '',
-    ),
-    OboardContent(
-      title:
-          'Рекомендации по кандидатам и флоу их ведения теперь в одном месте',
-      description: '',
-      button: '',
-    ),
-    OboardContent(
-      title: '-Вакансия -\n- рекомендация -\n- трудоустройство -',
-      description: '',
-      button: 'К регистрации',
-    ),
+  final List<Widget> _onboardingPages = [
+    const MegaPhone(),
+    const Settings(),
+    const Contract(),
   ];
 
   int _currentPage = 0;
@@ -38,39 +26,89 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            Expanded(
-              child: PageView.builder(
-                itemCount: _onboardingPages.length,
-                controller: _pageController,
-                onPageChanged: (currentPage) {
-                  setState(() {
-                    _currentPage = currentPage;
-                  });
-                },
-                itemBuilder: (context, index) => OnboardingPage(
-                  title: _onboardingPages[index].title,
-                  description: _onboardingPages[index].description,
-                  button: _onboardingPages[index].button,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 60,
-              width: 60,
-              child: Row(
-                children: List<Widget>.generate(
-                  _onboardingPages.length,
-                  (index) => Padding(
-                    padding: const EdgeInsets.only(right: 5),
-                    child: DotIndicator(
-                      isActive: index == _currentPage,
-                    ),
-                  ),
-                ),
-              ),
-            ),
+            pageBody(),
+            nextButton(context),
+            dotIndicator(),
           ],
         ),
+      ),
+    );
+  }
+
+  MaterialButton nextButton(BuildContext context) {
+    return MaterialButton(
+      onPressed: () {
+        if (_currentPage == _onboardingPages.length - 1) {
+          Navigator.of(context)
+              .pushNamedAndRemoveUntil('/authorization', (route) => false);
+        }
+        setState(() {
+          _currentPage++;
+        });
+        setState(() {
+          _pageController.animateToPage(_currentPage,
+              duration: const Duration(milliseconds: 500),
+              curve: Curves.easeInOut);
+        });
+      },
+      child: Container(
+        width: 320,
+        height: 36,
+        decoration: BoxDecoration(
+          gradient: gradient,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            _currentPage != _onboardingPages.length - 1
+                ? "Далее"
+                : "К регистрации",
+            style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  SizedBox dotIndicator() {
+    return SizedBox(
+      height: 60,
+      width: 60,
+      child: Row(
+        children: List<Widget>.generate(
+          _onboardingPages.length,
+          (index) => Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: DotIndicator(
+              isActive: index == _currentPage,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Flexible pageBody() {
+    return Flexible(
+      flex: 1,
+      fit: FlexFit.tight,
+      child: PageView.builder(
+        itemCount: _onboardingPages.length,
+        controller: _pageController,
+        onPageChanged: (currentPage) {
+          setState(() {
+            _currentPage = currentPage;
+          });
+        },
+        itemBuilder: (context, index) {
+          return AnimatedOpacity(
+            opacity: _currentPage == index ? 1 : 0,
+            duration: const Duration(milliseconds: 500),
+            child: _onboardingPages[index],
+          );
+        },
       ),
     );
   }
